b6d70e4811ac259eb247afd0c72bf306a99a44d4
Carlos de Tena Bellmont <carlos.bellmont@gmail.com>
2020-11-17 18:38:31 +0100

    Ganador fixed

--- src/com/cbellmont/Cuadrilatero.java
+++ src/com/cbellmont/Cuadrilatero.java
@@ -12,13 +12,21 @@ public class Cuadrilatero {
     public static void addParticipante(Luchador luchador) throws InterruptedException {
         available.acquire();
         System.out.println("Entra al ring el luchador " + luchador.getName());
-        if (ganador == null ) {
-            ganador = luchador;
-        } else {
+        if (!soyPrimero(luchador)) {
             System.out.println("Comienza la pelea entre " + luchador.getName() + " y " + ganador.getName());
             Thread.sleep(2000);
             pelear(ganador, luchador);
         }
+    }
+
+    private static synchronized boolean soyPrimero(Luchador luchador) throws InterruptedException {
+        if (ganador == null){
+            ganador = luchador;
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
     private static void pelear(Luchador ganador, Luchador luchador) {
